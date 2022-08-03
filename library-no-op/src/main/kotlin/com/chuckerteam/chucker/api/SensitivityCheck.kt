d0851c24e32190a6eecf97d13c438c8a78099aea
Putu Wahyu <putu.permadi@grabtaxi.com>
2022-08-03 22:42:57 +0800

    add regex filter

---
+++ library-no-op/src/main/kotlin/com/chuckerteam/chucker/api/SensitivityCheck.kt
@@ -0,0 +1,46 @@
+package com.chuckerteam.chucker.api
+
+/**
+ * Created by putu.permadi on 03/08/22.
+ */
+
+public class SensitivityCheck(private val builder: Builder) {
+
+    internal fun isCheckHostSensitive(host: String?): Boolean {
+        return false
+    }
+
+    internal fun isCheckResponseBodySensitive(response: String?): Boolean {
+        return false
+    }
+
+    internal fun isCheckPathSensitive(path: String?): Boolean {
+        return false
+    }
+
+    internal fun isRequestBodySensitive(requestBody: String?): Boolean {
+        return false
+    }
+
+    private fun isSensitive(text: CharSequence?): Boolean {
+        return false
+    }
+
+    public class Builder(regex: Regex) {
+        internal var isCheckHostSensitivity: Boolean = false
+        internal var isCheckPathSensitivity: Boolean = false
+        internal var isCheckResponseBodySensitivity: Boolean = false
+        internal var isCheckRequestBodySensitivity: Boolean = false
+        internal var regexValidator = regex
+
+        public fun isCheckHostSensitivity(enable: Boolean): Builder = this
+
+        public fun isCheckPathSensitivity(enable: Boolean): Builder = this
+
+        public fun isCheckResponseBodySensitivity(enable: Boolean): Builder = this
+
+        public fun isCheckRequestBodySensitivity(enable: Boolean): Builder = this
+
+        public fun build(): SensitivityCheck = SensitivityCheck(this)
+    }
+}
